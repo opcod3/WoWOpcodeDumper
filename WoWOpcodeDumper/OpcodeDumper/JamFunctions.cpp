423b0042847eafb4d0d5e210824c2d34377ba2de
Zerix <tkalmbach18@gmail.com>
2015-04-20 15:14:37 -0400

    WoWOpcodeDumper: Holy spacing Batman

--- WoWOpcodeDumper/OpcodeDumper/JamFunctions.cpp
+++ WoWOpcodeDumper/OpcodeDumper/JamFunctions.cpp
@@ -1,5 +1,76 @@
 #include "JamFunctions.h"
 
+JamData GetData(uint8* addr)
+{
+    JamData data;
+    data.ctor = 0;
+    data.callHandler = 0;
+
+    // Iterates through the client to find the JamData.
+    for (int i = 0; i < 50; i++)
+    {
+        if (addr[i] == 0xE8 && addr[i + 5] == 0x8D)
+        {
+            // If the address iteration + 6 is LEA and address iteration + 11 is Call
+            if (addr[i + 6] == 0x8D && addr[i + 11] == 0xE8)
+            {
+                // Finds the data based on the address for ctor and call handler.
+                JamData data;
+                data.ctor = ((uint32)addr + i) + (*(uint32*)(addr + i + 1)) + 5;
+                data.callHandler = ((uint32)addr + i + 11) + (*(uint32*)(addr + i + 12)) + 5;
+                data.startsAt = ((uint32)addr) + i;
+                int additionalLen = 0;
+
+                // LEA
+                if (addr[i + 16] == 0x8D)
+                {
+                    if (addr[i + 17] == 0x8D)
+                        additionalLen = 6;
+                    else
+                        additionalLen = 3;
+                    if (addr[i + 16 + additionalLen] == 0xE8)
+                        additionalLen += 5;
+                }
+
+                data.len = 15 + i + 1 + additionalLen;
+                data.addr = (uint32)addr;
+                return data;
+            }
+
+            // If the address iteration +6 is 0x4D and address iteration + 8 is Call
+            if (addr[i + 6] == 0x4D && addr[i + 8] == 0xE8)
+            {
+                // Finds the data based on the address for ctor and call handler.
+                JamData data;
+                data.ctor = ((uint32)addr + i) + (*(uint32*)(addr + i + 1)) + 5;
+                data.callHandler = ((uint32)addr + i + 8) + (*(uint32*)(addr + i + 9)) + 5;
+                data.startsAt = ((uint32)addr) + i;
+                int additionalLen = 0;
+
+                // LEA
+                if (addr[i + 13] == 0x8D)
+                {
+                    if (addr[i + 14] == 0x8D)
+                        additionalLen = 6;
+                    else
+                        additionalLen = 3;
+
+                    if (addr[i + 13 + additionalLen] == 0xE8)
+                        additionalLen += 5;
+                }
+
+                data.len = 12 + i + 1 + additionalLen;
+                data.addr = (uint32)addr;
+                return data;
+            }
+        }
+    }
+
+    // Cannot obtain address
+    assert("Fatal error at getting addr" && false);
+    return data;
+}
+
 bool GroupVtable::IsJam(__int16 opcode)
 {
 	pIsJam _isJam = (pIsJam)(isJam);
